8d40b63df917e76bcb8d61edba6e9b8d712d9ffc
Marcus <liu.mingku@northeastern.edu>
2023-08-28 16:54:15 -0400

    Grouped dependencies

--- premake5.lua
+++ premake5.lua
@@ -25,9 +25,12 @@ IncludeDir["GLFW"] = "Hazel/vendor/GLFW/include"
 IncludeDir["Glad"] = "Hazel/vendor/Glad/include"
 IncludeDir["ImGui"] = "Hazel/vendor/imgui"
 
-include "Hazel/vendor/GLFW"
-include "Hazel/vendor/Glad"
-include "Hazel/vendor/imgui"
+group "Dependencies"
+	include "Hazel/vendor/GLFW"
+	include "Hazel/vendor/Glad"
+	include "Hazel/vendor/imgui"
+
+group ""
 
 project "Hazel"
 	-- Because we want to put part of the things into the Hazel file inside the outer Hazel file.
